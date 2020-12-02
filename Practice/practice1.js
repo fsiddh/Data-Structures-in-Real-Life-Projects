c4769fc8805da01cde20af2833edf1b823fc163c
fsiddh <jasonshaw546@gmail.com>
2020-12-02 23:12:11 +0530

    practice

---
+++ Practice/practice1.js
@@ -0,0 +1,11 @@
+class test{
+    constructor(color, shape){
+        this.color = color;
+        this.shape = shape;
+    }
+};
+
+
+
+var c = document.getElementById('my_canvas');
+console.log(c);

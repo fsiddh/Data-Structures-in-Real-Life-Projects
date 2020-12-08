be2a22b4de64690a410550b96882f31caea1a2ab
fsiddh <jasonshaw546@gmail.com>
2020-12-08 16:11:54 +0530

    update 0.1

---
+++ 2. Snake Game(OOPs)/snake.js
@@ -0,0 +1,43 @@
+function init(){
+    canvas = document.getElementById('mycanvas');
+    W = H = canvas.width = canvas.height = 900;
+    pen = canvas.getContext('2d');
+    cell_size = 50;
+
+    snake = {
+        init_len: 4,
+        color: "blue",
+        cells: [],
+        direction: "right",
+
+        createSnake: function(){
+            for (var i = this.init_len; i<0; i--){
+                this.cells.push({x:i, y:0});
+            }
+        },
+
+        drawSnake: function(){
+            for(var i=0; i<this.cells.length; i++){
+                pen.fillRect(this.cells[i].x, this.cells[i].y, cell_size, cell_size);
+            }
+        }
+    };
+
+    snake.createSnake();
+}
+
+function draw(){
+    snake.drawSnake();
+}
+
+function update(){
+
+}
+
+function gameloop(){
+    draw();
+    update();
+}
+
+init();
+var f = setInterval(gameloop, 100);

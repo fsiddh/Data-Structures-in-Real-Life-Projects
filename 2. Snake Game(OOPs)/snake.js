d4ea64f97c92dd21c18233b73a94e680931b8768
fsiddh <jasonshaw546@gmail.com>
2020-12-08 23:12:32 +0530

    food and boundary update

--- 2. Snake Game(OOPs)/snake.js
+++ 2. Snake Game(OOPs)/snake.js
@@ -2,7 +2,9 @@ function init(){
     canvas = document.getElementById('mycanvas');
     W = H = canvas.width = canvas.height = 900;
     pen = canvas.getContext('2d');
-    cell_size = 67;
+    cell_size = 66;
+    food = getrandomfood();
+    game_over = false;
 
     snake = {
         init_len: 4,
@@ -11,23 +13,31 @@ function init(){
         direction: "right",
 
         createSnake: function(){
-            for (var i = this.init_len; i>=0; i--){
+            for (var i = this.init_len; i>0; i--){
                 this.cells.push({x:i, y:0});
             }
         },
 
         drawSnake: function(){
             for(var i=0; i<this.cells.length; i++){
+                pen.fillStyle = this.color;
                 pen.fillRect(this.cells[i].x*cell_size, this.cells[i].y*cell_size, cell_size-2, cell_size-2);
             }
         },
 
         updateSnake: function(){
             console.log("Updating Snake. ");
-            this.cells.pop();
-            
+
             var headX = this.cells[0].x;
             var headY = this.cells[0].y;
+
+            if (headX == food.x && headY == food.y){
+                food = getrandomfood();
+            }
+            else{
+                this.cells.pop();
+            }
+            
             var nextX, nextY;
 
             if (snake.direction == 'right'){
@@ -48,11 +58,20 @@ function init(){
             }
 
             this.cells.unshift({x:nextX, y:nextY});
+
+            /*Write a Logic that prevents snake from going out*/
+            var lastX = Math.round(W/cell_size);
+            var lastY = Math.round(H/cell_size);
+
+            if (this.cells[0].x > lastX || this.cells[0].y > lastY || 
+                this.cells[0].x < 0 || this.cells[0].y < 0){
+                game_over = true;
+            }
         }
     };
 
     snake.createSnake();
-
+    //Add a Event Listener on the Document Object
     function keyPressed(e){
         if (e.key == 'ArrowRight'){
             snake.direction = 'right';
@@ -72,15 +91,37 @@ function init(){
 }
 
 function draw(){
+    //erase the old frame
     pen.clearRect(0,0,W,H);
     snake.drawSnake();
+
+    pen.fillStyle = food.color;
+    pen.fillRect(food.x*cell_size, food.y*cell_size, cell_size, cell_size);
 }
 
 function update(){
     snake.updateSnake();
 }
 
+function getrandomfood(){
+    var foodX = Math.round( Math.random()*(W-cell_size) / cell_size);
+    var foodY = Math.round( Math.random()*(H-cell_size) / cell_size);
+
+    var food = {
+        x: foodX,
+        y: foodY,
+        color: "red",
+    }
+
+    return food;
+}
+
 function gameloop(){
+    if (game_over == true){
+        clearInterval(f);
+        alert("Game Over");
+        return;
+    }
     draw();
     update();
 }

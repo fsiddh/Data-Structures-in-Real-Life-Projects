50b86b1ff703402285af2b5836589d5231f4810d
fsiddh <jasonshaw546@gmail.com>
2020-12-08 19:23:32 +0530

    direction update

--- 2. Snake Game(OOPs)/snake.js
+++ 2. Snake Game(OOPs)/snake.js
@@ -11,7 +11,7 @@ function init(){
         direction: "right",
 
         createSnake: function(){
-            for (var i = this.init_len; i>0; i--){
+            for (var i = this.init_len; i>=0; i--){
                 this.cells.push({x:i, y:0});
             }
         },
@@ -20,18 +20,64 @@ function init(){
             for(var i=0; i<this.cells.length; i++){
                 pen.fillRect(this.cells[i].x*cell_size, this.cells[i].y*cell_size, cell_size-2, cell_size-2);
             }
+        },
+
+        updateSnake: function(){
+            console.log("Updating Snake. ");
+            this.cells.pop();
+            
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
+            var nextX, nextY;
+
+            if (snake.direction == 'right'){
+                nextX = headX + 1;
+                nextY = headY;
+            }
+            else if (snake.direction == 'left'){
+                nextX = headX - 1;
+                nextY = headY;
+            }
+            else if (snake.direction == 'down'){
+                nextX = headX;
+                nextY = headY + 1;
+            }
+            else{
+                nextX = headX;
+                nextY = headY - 1;
+            }
+
+            this.cells.unshift({x:nextX, y:nextY});
         }
     };
 
     snake.createSnake();
+
+    function keyPressed(e){
+        if (e.key == 'ArrowRight'){
+            snake.direction = 'right';
+        }
+        else if (e.key == 'ArrowLeft'){
+            snake.direction = 'left';
+        }
+        else if (e.key == 'ArrowDown'){
+            snake.direction = 'down';
+        }
+        else{
+            snake.direction = 'up';
+        }
+    }
+
+    document.addEventListener('keydown', keyPressed);
 }
 
 function draw(){
+    pen.clearRect(0,0,W,H);
     snake.drawSnake();
 }
 
 function update(){
-
+    snake.updateSnake();
 }
 
 function gameloop(){

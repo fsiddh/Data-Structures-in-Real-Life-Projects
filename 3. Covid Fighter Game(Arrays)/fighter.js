237d01ecaa4848d4eb198227d36ef8f8696e7021
fsiddh <jasonshaw546@gmail.com>
2020-12-09 20:50:52 +0530

    Completed

--- 3. Covid Fighter Game(Arrays)/fighter.js
+++ 3. Covid Fighter Game(Arrays)/fighter.js
@@ -20,6 +20,7 @@ function init(){
 
     // To draw on canvas
     pen = canvas.getContext("2d");
+    game_over = false;
 
     e1 = {
 		x : 150,
@@ -48,15 +49,15 @@ function init(){
     player = {
 		x : 20,
 		y : H/2,
-		w : 60,
-		h : 60,
+		w : 50,
+		h : 50,
 		speed : 20,
         moving  : false,
         health : 100,
 	};
     
 	gem = {
-		x : W-100,
+		x : W-70,
 		y : H/2,
 		w : 60,
 		h : 60,
@@ -71,6 +72,18 @@ function init(){
     })
 }
 
+function isOverlap(rect1,rect2){
+    if (rect1.x < rect2.x + rect2.w &&
+   rect1.x + rect1.w > rect2.x &&
+   rect1.y < rect2.y + rect2.h &&
+   rect1.y + rect1.h > rect2.y) {
+    return true
+    }
+    
+    return false;
+    
+}
+
 function draw(){
     // To erase previous frame
     pen.clearRect(0, 0, W, H);
@@ -79,17 +92,52 @@ function draw(){
         pen.drawImage(enemy_img, enemy[i].x, enemy[i].y, enemy[i].w, enemy[i].h);
     }
 
+    //draw the player
     pen.drawImage(player_img, player.x, player.y, player.w, player.h);
 
+    //draw the gem
     pen.drawImage(gem_img, gem.x, gem.y, gem.w, gem.h);
+
+    // Display the Score
+    for(let i=0;i<enemy.length;i++){
+        pen.drawImage(enemy_img,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+    }
+    
+    pen.fillStyle = "white";
+    pen.fillText("Score "+player.health,10,10);
 }
 
 function update(){
 
+    //if the player is moving 
     if (player.moving == true){
         player.x += player.speed;
     }
 
+    for(let i=0;i<enemy.length;i++){
+        if(isOverlap(enemy[i],player)){
+            player.health -= 50;
+            if(player.health <0){
+                console.log(player.health);
+                game_over = true;
+                alert("Game Over" + player.health);
+            }
+        }
+    }
+    
+   
+    
+    //overlap overlap
+    if(isOverlap(player,gem)){
+        
+        console.log("You Won");
+        alert("You Won!");
+        game_over = true;
+        return;
+    }
+
+    // move the box downwards
+    // update each enemy by same logic
     for (var i = 0; i < enemy.length; i++){
         enemy[i].y += enemy[i].speed;
 
@@ -100,6 +148,9 @@ function update(){
 }
 
 function gameloop(){
+    if(game_over == true){
+        clearInterval(f);
+    }
     draw();
     update();
 }

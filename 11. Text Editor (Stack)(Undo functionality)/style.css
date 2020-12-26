4e653754706797161ecb3b5b2094c0240dd7917d
fsiddh <jasonshaw546@gmail.com>
2020-12-26 14:43:31 +0530

    some styling

---
+++ 11. Text Editor (Stack)(Undo functionality)/style.css
@@ -0,0 +1,32 @@
+html,
+body {
+    height: 100%;
+}
+
+.text_box {
+    width: 50%;
+    height: 100%;
+    border: 1px solid lightgray;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+}
+
+#container{
+    width: 100%;
+    height: 70%;
+    background-color: white;
+    display: flex;
+    margin: 0 auto;
+}
+
+#undo{
+    margin:auto;
+    display:block;
+}
+
+
+#clear{
+    margin:auto;
+    display:block;
+}

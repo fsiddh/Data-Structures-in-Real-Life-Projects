f7658b628ba099b203e8650eb1a2c4778b78bdf6
Falansh Siddh <jasonshaw546@gmail.com>
2021-07-29 21:38:31 +0530

    Create readme.js

---
+++ 17. Mario_Game/readme.js
@@ -0,0 +1,10 @@
+## Mario
+### Description
+Help Mario to collect maximum coins.
+
+### Skills nurtured:
+  - Graph.
+  - Used Dynamic Programming & Greedy Approach to get maximum coins at each level.
+
+### Tech. Stack:
+HTML, CSS, JS, CANVAS, PHASER
